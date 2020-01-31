1fee5f858a41734c4d877db385f0b5ff995d36cd
KishorNaik <kishor.naik011.net@gmail.com>
2020-01-31 15:05:24 +0530

    Code Commit

---
+++ Sol_EF_Core/Sol_Test/StoredProcedures/uspGetUsersMultiResultSet.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROC uspGetUsersMultiResultSet
+	@UserId numeric(18,0)
+AS
+	BEGIN
+		
+		SELECT * FROM tblUsers WHERE UserId=@UserId
+
+		SELECT * FROM tblUserLogin WHERE UserId=@UserId
+
+	END
