0321230b46214730c714c8d9125e700880df60c4
KishorNaik <kishor.naik011.net@gmail.com>
2020-01-31 11:16:43 +0530

    Code Commit

---
+++ Sol_EF_Core/Sol_Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sol_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
