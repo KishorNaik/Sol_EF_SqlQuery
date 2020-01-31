7ba582d2c6fefbda3d537817e364801487d5fb53
KishorNaik <kishor.naik011.net@gmail.com>
2020-01-31 15:57:54 +0530

    Code Commit

--- Sol_EF_Core/Sol_Test/Program.cs
+++ Sol_EF_Core/Sol_Test/Program.cs
@@ -7,6 +7,11 @@ namespace Sol_Test
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            // If you are using .Net Core 2.2 then Select EF_Version_2.2.6 Branch
+
+            // If You are using .Net Core 3.1 then Select EF_Version_3.3.1 Branch
+
         }
     }
 }
